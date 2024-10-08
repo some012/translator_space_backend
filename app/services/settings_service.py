396d64e58c8d9a617d0c60a4668f6794383eca25
m.arestov <m.arestov@estesis.tech>
2024-10-08 18:48:40 +0700

    feat(App): Added initial_data

--- app/services/settings_service.py
+++ app/services/settings_service.py
@@ -4,12 +4,17 @@ from fastapi import Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.deps.db import get_db
+from app.models import SettingsModel
+from app.schemas.settings import SettingsCreate
 from app.services.repositories.settings_repository import SettingsRepository
 
 
 class SettingsService:
     def __init__(self, db: AsyncSession):
         self._settings_repository = SettingsRepository(db=db)
+
+    async def create_settings(self, settings: SettingsCreate) -> SettingsModel:
+        return await self._settings_repository.create(c_obj=settings)
 
     @staticmethod
     def register(db: AsyncSession):
